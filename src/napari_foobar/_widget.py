9e78fdb0715dfe95199378e022cec716c8ec43dc
Gabriel Selzer <gjselzer@wisc.edu>
2022-03-08 12:02:08 -0600

    Provide an environment.yml
    Note that you'll still have to install this directory with pip :(

--- src/napari_foobar/_widget.py
+++ src/napari_foobar/_widget.py
@@ -7,7 +7,10 @@ see: https://napari.org/plugins/stable/guides.html#widgets
 Replace code below according to your needs.
 """
 from typing import Optional
+import jpype
 from magicgui import magic_factory
+
+jpype.startJVM()
 
 @magic_factory(img_layer = {"choices": ['a', 'b']}, max=None)
 def example_magic_widget(img_layer: Optional[str] = None):
