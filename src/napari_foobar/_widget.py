06ae73ee08624366261976d753970daf1108e0a9
Gabriel Selzer <gjselzer@wisc.edu>
2022-03-08 11:53:51 -0600

    Make the problem simpler

--- src/napari_foobar/_widget.py
+++ src/napari_foobar/_widget.py
@@ -7,46 +7,8 @@ see: https://napari.org/plugins/stable/guides.html#widgets
 Replace code below according to your needs.
 """
 from typing import Optional
-import jpype
-from qtpy.QtWidgets import QWidget, QHBoxLayout, QPushButton
-from magicgui import magic_factory, magicgui
+from magicgui import magic_factory
 
-
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # in one of two ways:
-    # 1. use a parameter called `napari_viewer`, as done here
-    # 2. use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, napari_viewer):
-        super().__init__()
-        self.viewer = napari_viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
-
-
-@magic_factory
-def example_magic_widget(img_layer: "napari.layers.Image"):
-    jpype.startJVM()
-    System = jpype.JClass('java.lang.System')
-    print(System.nanoTime())
-
-@magic_factory(img_layer = {"choices": ['a', 'b'], "max": None})
-def example_magic_widget2(img_layer: Optional[str] = None):
-    print(f"you have selected {img_layer}")
-
-@magic_factory()
-def example_magic_widget3(img_layer: Optional[str] = None):
-    magicgui(function=[]) # Should fail
-
-# Uses the `autogenerate: true` flag in the plugin manifest
-# to indicate it should be wrapped as a magicgui to autogenerate
-# a widget.
-def example_function_widget(img_layer: "napari.layers.Image"):
+@magic_factory(img_layer = {"choices": ['a', 'b']}, max=None)
+def example_magic_widget(img_layer: Optional[str] = None):
     print(f"you have selected {img_layer}")
